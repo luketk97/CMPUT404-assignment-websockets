3bd1958b3cf4d54f12c813471acaf8d367485ada
luketk97 <taekun@ualberta.ca>
2021-03-24 15:29:38 -0600

    assignment 5

--- sockets.py
+++ sockets.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect
 from flask_sockets import Sockets
 import gevent
 from gevent import queue
@@ -25,6 +25,28 @@ import os
 app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
+
+clients = list()
+
+'''Source: CMPUT404 class source code'''
+def send_all(msg):
+    for client in clients:
+        client.put( msg )
+
+'''Source: CMPUT404 class source code'''
+def send_all_json(obj):
+    send_all( json.dumps(obj) )
+
+'''Source: CMPUT404 class source code'''
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+    
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
 
 class World:
     def __init__(self):
@@ -63,25 +85,52 @@ myWorld = World()
 
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    ''' sets up a new object based on the given data and send
+        JSONfied object to client                         '''
+    update = {entity:data}
+    send_all_json(update)
 
 myWorld.add_set_listener( set_listener )
         
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect("/static/index.html", code=302)
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
-    return None
+    '''Source: CMPUT404 class source code'''
+    try:
+        while True:
+            msg = ws.receive()
+            print("WS RECV: %s" % msg)
+            if (msg is not None):
+                packet = json.loads(msg)
+                send_all_json( packet )
+            else:
+                break
+    except:
+        print("Done")
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
+    '''Source: CMPUT404 class source code'''
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn( read_ws, ws, client)
+    try:
+        while True:
+            msg = client.get()
+            ws.send(msg)
+    except Exception as e:
+        print("WS Error %s" % e)
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
